16581027835e7bbcd351effdc6f9c862cf9c5094
liranbr <liranbraverman@gmail.com>
2024-09-24 17:05:52 +0300

    Made Modal for viewing/editing games

--- src/GameGrid.jsx
+++ src/GameGrid.jsx
@@ -1,4 +1,6 @@
 import "./GameGrid.css";
+import { Button, Modal } from "react-bootstrap";
+import { useState } from "react";
 
 const games = [
     "Baldur's Gate 3",
@@ -18,15 +20,57 @@ const games = [
     "The Witcher 3",
 ];
 
-const GameCard = (game) => (
-    <button key={game} className="game-card" onClick={() => {}}>
-        <img
-            draggable="false"
-            src={`/cards/${game}.png`}
-            alt={`${game} Game Cover`}
-        />
-    </button>
-);
+function GameCard(game) {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <button key={game} className="game-card" onClick={handleShow}>
+                <img
+                    draggable="false"
+                    src={`/cards/${game}.png`}
+                    alt={`${game} Game Cover`}
+                />
+            </button>
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>{game}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body style={{ display: "flex", flexDirection: "row" }}>
+                    <img
+                        draggable="false"
+                        src={`/cards/${game}.png`}
+                        alt={`${game} Game Cover`}
+                        style={{
+                            width: "100%",
+                            height: "100%",
+                            marginRight: "10px",
+                        }}
+                    />
+                    <div>
+                        <p>
+                            [Placeholder for Form about game details,
+                            categories, friends, etc]
+                        </p>
+                        <div
+                            style={{
+                                position: "absolute",
+                                bottom: "1rem",
+                                right: "1rem",
+                            }}
+                        >
+                            <Button variant="primary" onClick={handleClose}>
+                                Save
+                            </Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+}
 
 export function GamesGrid() {
     return (

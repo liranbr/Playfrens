1ee62423ddba22c745418ee7f32f1290a259a76b
Sami Shakkour <Sami.shakkour@hotmail.com>
2025-08-15 14:08:35 +0300

    removed debugging console logs

--- src/components/Dialogs/EditGameDialog.jsx
+++ src/components/Dialogs/EditGameDialog.jsx
@@ -48,7 +48,6 @@ export function EditGameDialog({ open, closeDialog, game = null }) {
         if (timer.current) clearTimeout(timer.current);
         timer.current = setTimeout(() => {
             setGameName(value);
-            console.log("debounced value:", value);
         }, 1000); // adjust debounce delay as needed
     };
 
@@ -111,7 +110,7 @@ export function EditGameDialog({ open, closeDialog, game = null }) {
 function SteamGridDBImages({ gameName, gameCoverInputRef }) {
     const [images, setImages] = useState([]);
     const [error, setError] = useState("");
-    console.log("gameName", gameName);
+
     useEffect(() => {
         if (!gameName) return;
         fetch(`/api/steamgriddb/getGrids/${encodeURIComponent(gameName)}`)
@@ -123,7 +122,6 @@ function SteamGridDBImages({ gameName, gameCoverInputRef }) {
             .catch(err => setError(err.message), setImages([]));
     }, [gameName]);
 
-    console.log(images);
     if (error) return <div>Error: {error}</div>;
     if (images.length == 0) return <></>
     return (

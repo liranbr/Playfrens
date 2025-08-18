4c9b08967dcc62676c96bb98c9ec4395a7249719
Sami Shakkour <Sami.shakkour@hotmail.com>
2025-08-18 12:55:07 +0300

    removed unused responses code

--- backend/service.js
+++ backend/service.js
@@ -14,23 +14,4 @@ export class Service {
     listen() {
         console.log(`Listening to ${this.constructor.name}`);
     }
-
-    /** @param {import('express').Response} res */
-    sendOk = (res, data = {}) => {
-        res.status(200).json(data);
-    };
-
-    /** @param {import('express').Response} res */
-    sendNotFound = (res, resourceName, value) => {
-        res.status(404).json({
-            message: `Couldn't find any ${resourceName} matching ${value ? `name: ${value}` : "criteria"}`,
-        });
-    };
-    /** @param {import('express').Response} res */
-    sendError = (
-        res,
-        message = { message: "An error has occured while processing your request!" },
-    ) => {
-        res.status(500).json(message);
-    };
 }

065c9fc81f2d6f7877c41b7c50cf019131ee0617
liranbr <liranbraverman@gmail.com>
2025-08-28 21:40:44 +0300

    Reset ScrollViews to top on reload

--- src/components/common/ScrollView.jsx
+++ src/components/common/ScrollView.jsx
@@ -1,4 +1,5 @@
 import * as ScrollArea from "@radix-ui/react-scroll-area";
+import { useEffect, useRef } from "react";
 
 /**
  * A vertically scrollable view component. Should put a div inside it if styling is needed.
@@ -17,13 +18,22 @@ export function ScrollView({
     children,
     ...rest
 }) {
+    const viewportRef = useRef(null);
+    useEffect(() => {
+        if (viewportRef.current) viewportRef.current.scrollTop = 0;
+    }, []); // On mount or reload, force scroll back to top, only once
+
     return (
         <ScrollArea.Root
             type={type || "auto"}
             className={`rx-scroll-area ${rootClassName}`}
             {...rest}
         >
-            <ScrollArea.Viewport asChild className={`rx-scroll-viewport ${viewportClassName}`}>
+            <ScrollArea.Viewport
+                ref={viewportRef}
+                asChild
+                className={`rx-scroll-viewport ${viewportClassName}`}
+            >
                 {children}
             </ScrollArea.Viewport>
             <ScrollArea.Scrollbar orientation="vertical" className="rx-scrollbar">

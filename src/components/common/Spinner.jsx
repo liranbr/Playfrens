509352c25c52068141f060959f4bd9362c2b8b69
liranbr <liranbraverman@gmail.com>
2025-08-29 13:06:01 +0300

    Add Spinner to EditGameDialog while typing / loading covers

---
+++ src/components/common/Spinner.jsx
@@ -0,0 +1,6 @@
+import "./Spinner.css";
+import { FaSpinner } from "react-icons/fa";
+
+export function Spinner() {
+    return <FaSpinner className="spinner" aria-label="Loading" role="status" />;
+}

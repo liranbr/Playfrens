0b33a18ee644d7c140424c5ed91d8b454ff91ca1
liranbr <liranbraverman@gmail.com>
2025-11-05 02:05:10 +0200

    Stylize, clean, and rephrase the Welcome Tour (WIP)

--- src/components/index.js
+++ src/components/index.js
@@ -11,3 +11,4 @@ export { SidebarTagButton } from "./TagButton.jsx";
 export { SidebarTagButtonGroup } from "./TagButtonGroup.jsx";
 export { ReminderCard } from "./ReminderCard.jsx";
 export { DialogRoot } from "./Dialogs/DialogRoot.jsx";
+export { WelcomeTour } from "./WelcomeTour.jsx";

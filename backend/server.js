31064e6868e3312ccd7a94e55278d06b5547fd64
liranbr <liranbraverman@gmail.com>
2025-11-25 13:05:05 +0200

    Express JSON - Set explicit board size limit instead of the default 100kb

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPAB
 const app = express();
 app.set("trust proxy", 1);
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 const isProd = process.env.NODE_END === "production";

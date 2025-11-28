0f542a257853a4041cff0a2a8bf9c42071a61987
liranbr <liranbraverman@gmail.com>
2025-11-28 11:55:07 +0200

    Login page - add temp local backup button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import "./Login.css";
 import { Button, SimpleTooltip } from "@/components";
 import { BiLogoDiscordAlt, BiLogoGoogle, BiLogoSteam } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import { backupToFile } from "@/stores/index.js";
 
 export default function Login() {
     return (
@@ -12,8 +13,12 @@ export default function Login() {
                     <span>to use Playfrens</span>
                 </div>
                 {/* TODO: make login buttons work, and they then route you to /app */}
+                {/* TODO: after testers backup, remove the button */}
                 <div className="continue-with">
                     <div className="auth-buttons">
+                        <Button variant="danger" onClick={backupToFile}>
+                            (Testers - Backup local data before logging in)
+                        </Button>
                         <Button variant="secondary">
                             <BiLogoSteam />
                             Continue with Steam
